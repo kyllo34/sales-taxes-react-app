190282ef993a3f1903268a699d1faefb49169cb7
Andrew Kyllo <kyllo34@gmail.com>
2020-05-25 17:03:38 -0700

    Updates

---
+++ precache-manifest.56659c0b49a8bafa6f1aac0c1a2cf451.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "793d35a58b31e2050d2591173879f99c",
+    "url": "/sales-taxes-react-app/index.html"
+  },
+  {
+    "revision": "93deec9f059ddf0a3fc5",
+    "url": "/sales-taxes-react-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "352889688011a5863564",
+    "url": "/sales-taxes-react-app/static/css/main.5715f891.chunk.css"
+  },
+  {
+    "revision": "93deec9f059ddf0a3fc5",
+    "url": "/sales-taxes-react-app/static/js/2.a07556d2.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/sales-taxes-react-app/static/js/2.a07556d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352889688011a5863564",
+    "url": "/sales-taxes-react-app/static/js/main.6b14e1a1.chunk.js"
+  },
+  {
+    "revision": "c5c25666ea0aff540ce1",
+    "url": "/sales-taxes-react-app/static/js/runtime-main.da3426d7.js"
+  }
+]);
